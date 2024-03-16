3a887abfb8ca25922baf02a637ca3ca97130c93f
ahmadhabibshovo <ahmadhabibshovo@gmail.com>
2024-03-16 21:19:50 +0600

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,29 @@
+void main() {
+  // Create a list of fruits represented as maps
+  List<Map<String, dynamic>> fruits = [
+    {"name": "Apple", "color": "Red", "price": 2.5},
+    {"name": "Banana", "color": "Yellow", "price": 1.0},
+    {"name": "Grapes", "color": "Purple", "price": 3.0},
+  ];
+  print("Original Fruit Details before Discount:");
+  displayFruitDetails(fruits);
+
+  double discount = 0.1;
+  applyPriceDiscount(fruits, discount);
+
+  print("\nFruit Details After Applying ${discount * 100}% Discount:");
+  displayFruitDetails(fruits);
+}
+
+
+void displayFruitDetails(List<Map<String, dynamic>> fruits) {
+  for (var fruit in fruits) {
+    print("Name: ${fruit["name"]}, Color: ${fruit["color"]}, Price: \$${fruit["price"]}");
+  }
+}
+
+void applyPriceDiscount(List<Map<String, dynamic>> fruits, double discount) {
+  for (var fruit in fruits) {
+    fruit["price"] = (fruit["price"] as double) * (1 - discount);
+  }
+}
